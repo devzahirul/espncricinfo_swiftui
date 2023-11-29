0841205f0eb214f5048f058d01a24adfae138b5c
Zahirul <jislam150@gmail.com>
2023-11-29 13:20:24 +0600

    Initial Commit

---
+++ MovieApp/MovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppApp.swift
+//  MovieApp
+//
+//  Created by Hologram1 on 11/29/23.
+//
+
+import SwiftUI
+
+@main
+struct MovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
